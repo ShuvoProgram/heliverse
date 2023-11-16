f923115f1655238b0dc1785947da5db9a9881fd1
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-17 02:24:39 +0600

    🌐 Established initial database connection setup.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import httpStatus from 'http-status'
-// import globalErrorHandler from './app/middleware/globalErrorHandler'
+import globalErrorHandling from './app/middleware/globalErrorHandling'
 // import router from './app/routes'
 
 const app: Application = express()
@@ -16,11 +16,11 @@ app.use(express.urlencoded({ extended: true }))
 
 // Testing route
 app.get('/api/v1', async (req: Request, res: Response) => {
-  res.send('Working Successfully')
+  res.send('Working Successfully 🚀')
 })
 
 // Global error handler (should be placed after route handlers)
-// app.use(globalErrorHandler)
+app.use(globalErrorHandling)
 
 // Handle not found routes (should be placed at the end)
 app.use((req: Request, res: Response, next: NextFunction) => {
